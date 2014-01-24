9145c054332e309e89dccd2a159fc2aff2a629e3
Apprentice <apprentice@dbc-chicago-160.local>
2014-01-24 13:53:17 -0600

    Create faker data in seed file

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+require 'faker'
+
+5.times { List.create!(:name => Faker::Commerce.product_name) }
+
+# 5 times create a list with fake name
+  # add 10 tasks to each list
+for List.all.each do |list|
+  10.times do {
+    list.tasks << Task.create(:description => Faker::Lorem.sentence)
+  }
+end
+
+
+#test to see if we can insert a todo into our database through our model using activerecord
+# Task.create(:description => Faker::Lorem.sentence, :complete => 0)
