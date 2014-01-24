599c5951900cf480f76acafc761702ad4fcc3c72
Brian Murphy <bmurphy1@mac.com>
2014-01-24 11:30:01 -0600

    Create task and list migrations

---
+++ db/migrate/20140124111400_create_tasks.rb
@@ -0,0 +1,12 @@
+
+
+class CreateTasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.string :description
+      t.integer :complete
+      t.integer :list_id
+      t.timestamps
+    end
+  end
+end
