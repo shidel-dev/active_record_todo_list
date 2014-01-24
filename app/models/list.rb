059a0702b4ee32eadb143bd5d6b3b51f3e82f873
Apprentice <apprentice@dbc-chicago-23.local>
2014-01-24 14:51:08 -0600

    .

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,10 +2,10 @@ class List < ActiveRecord::Base
   validates :name, presence: true
 
 
-  def add(desc)
-    self.tasks << Task.create(:description => desc :complete => 0)
-    return true
-  end
+  # def add_task(desc)
+  #   self.tasks << Task.create(:description => desc, :complete => 0)
+  #   return true
+  # end
 
   def delete(num)
 
