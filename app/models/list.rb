822f02590ae75c047c0609da9ec876dac21492a2
Apprentice <apprentice@dbc-chicago-23.local>
2014-01-24 14:43:01 -0600

    add methods to list model

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,16 @@
 class List < ActiveRecord::Base
   validates :name, presence: true
 
+  def add(desc)
+    self.tasks << Task.create(:description => desc :complete => 0)
+    return true
+  end
 
+  def delete(num)
+
+  end
+
+  def complete(num)
+
+  end
 end
